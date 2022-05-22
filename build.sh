163bd7e6807643f7f1b257dd8d3f92df09cf5861
Matthias Wild <mauwii@outlook.de>
2022-05-22 16:40:00 +0200

    add script to build and push container

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# STEP 1 - build images for two architectures
+export DOCKER_DEFAULT_PLATFORM="linux/amd64"
+docker build --build-arg='targetarch=linux-x64' --tag mauwii/devopsbuildagent --tag mauwii/devopsbuildagent:linux-x64 .
+export DOCKER_DEFAULT_PLATFORM="linux/arm64"
+docker build --build-arg='targetarch=linux-arm64' --tag mauwii/devopsbuildagent:linux-arm64 .
+
+# STEP 2 push images to registry
+docker push mauwii/devopsbuildagent
+docker push mauwii/devopsbuildagent:linux-x64
+docker push mauwii/devopsbuildagent:linux-arm64
+
+# STEP 3 - create and push manifest
+docker manifest create mauwii/devopsbuildagent:latest mauwii/devopsbuildagent:linux-x64 mauwii/devopsbuildagent:linux-arm64
+docker manifest annotate mauwii/devopsbuildagent:latest mauwii/devopsbuildagent:linux-x64 --os linux --arch amd64
+docker manifest annotate mauwii/devopsbuildagent:latest mauwii/devopsbuildagent:linux-arm64 --os linux --arch arm64 --variant v8
+docker manifest push --purge mauwii/devopsbuildagent:latest
