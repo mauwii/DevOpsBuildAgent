ef7da050582889052508b2e039cc5dd0924aa15b
Mauwii <mauwii@outlook.de>
2022-06-05 06:43:48 +0200

    highly improved build script

--- build.sh
+++ build.sh
@@ -1,33 +1,38 @@
 #!/usr/bin/env bash
+set -e
 
-# STEP 1 - build images for two architectures
-export DOCKER_DEFAULT_PLATFORM="linux/amd64"
-docker build \
-  --build-arg='TARGETARCH=linux-x64' \
-  --tag mauwii/devopsbuildagent \
-  --tag mauwii/devopsbuildagent:linux-x64 .
+# Step 1 - prepare
+export baseos='linux'
+export baseDistro='ubuntu'
+export baseVersion='18.04'
+export basetag='mauwii/devopsbuildagent'
 
-export DOCKER_DEFAULT_PLATFORM="linux/arm64"
-docker build \
-  --build-arg='TARGETARCH=linux-arm64' \
-  --tag mauwii/devopsbuildagent:linux-arm64v8 .
+build_image() {
+  export DOCKER_DEFAULT_PLATFORM="${baseos}/${targetproc}"
+  docker build \
+    --build-arg="targetarch=${baseos}-${targetarch}" \
+    --build-arg="baseimage=${targetproc}/${baseDistro}:${baseVersion}" \
+    --tag "${basetag}:${baseos}-${targetarch}" .
+}
 
-# STEP 2 push images to registry
-docker push mauwii/devopsbuildagent
-docker push mauwii/devopsbuildagent:linux-x64
-docker push mauwii/devopsbuildagent:linux-arm64v8
+# STEP 2 - build container images amd64 and arm64v8
+# begin with amd64
+export targetproc='amd64'
+export targetarch='x64'
+build_image
 
-# STEP 3 - create and push manifest
-docker manifest create mauwii/devopsbuildagent:latest \
-    mauwii/devopsbuildagent:linux-x64 \
-    mauwii/devopsbuildagent:linux-arm64v8 \
-  && docker manifest annotate \
-    mauwii/devopsbuildagent:latest \
-    mauwii/devopsbuildagent:linux-x64 \
-    --os linux --arch amd64 \
-  && docker manifest annotate \
-    mauwii/devopsbuildagent:latest \
-    mauwii/devopsbuildagent:linux-arm64v8 \
-    --os linux --arch arm64 --variant v8 \
-  && docker manifest push --purge \
-    mauwii/devopsbuildagent:latest
+# then build the arm64 image
+export targetarch='arm64'
+export targetproc="${targetarch}v8"
+build_image
+
+# STEP 3 push images to registry
+# docker push "${basetag}"
+docker push "${basetag}:${baseos}-x64"
+docker push "${basetag}:${baseos}-arm64v8"
+
+# STEP 4 - create and push manifest
+docker manifest create --amend ${basetag}:latest ${basetag}:${baseos}-x64 ${basetag}:${baseos}-arm64v8
+docker manifest annotate ${basetag}:latest ${basetag}:${baseos}-x64 --os ${baseos} --arch amd64
+docker manifest annotate ${basetag}:latest ${basetag}:${baseos}-arm64v8 --os ${baseos} --arch arm64 --variant v8
+docker manifest push --purge ${basetag}:latest
