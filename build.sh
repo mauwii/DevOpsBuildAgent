4210a9dd5026e1566ef50ecf11a62257ba7aef0e
Mauwii <mauwii@outlook.de>
2022-06-07 18:19:06 +0200

    huge update to build.sh

--- build.sh
+++ build.sh
@@ -1,32 +1,66 @@
 #!/usr/bin/env bash
 set -e
 
-# Step 1 - prepare
+# check command arguments if exists
+for argument in "$@"; do
+  # Disable Distribution - usefull when experimenting locally
+  [[ "$argument" = "--nodist" ]] && export nodist=1 && echo "distribution will be skipped \n"
+  # Dont build amd64 - usefull since this one is already working
+  [[ "$argument" = "--noamd64" ]] && export noamd64=1 && echo "Not building amd64 \n"
+  # Dont build arm64 - would be unfair to only opt-out amd64
+  [[ "$argument" = "--noarm64" ]] && export noarm64=1 && echo "Not building amd64 \n"
+done
+
+# Declare base-env
 export baseos='linux'
 export baseDistro='ubuntu'
 export baseVersion='20.04'
-export basetag='mauwii/devopsbuildagent'
+export dockerRegistry='docker.io'
+export dockerhubuser='mauwii'
+export dockerimage='devopsbuildagent'
+export dockerrepository="${dockerhubuser}/${dockerimage}"
+export builtTags=()
+export builtPlatformOs=()
+export builtPlatformArch=()
 
+# create build function
 build_image() {
-  export DOCKER_DEFAULT_PLATFORM="${baseos}/${targetproc}"
+  export DOCKER_DEFAULT_PLATFORM="${dockerdefaultplatformos:-$baseos}/${dockerdefaultplatformarch}"
+  export tag="${dockerrepository}:${dockerdefaultplatformos:-$baseos}.${baseDistro}.${baseVersion}.${dockerdefaultplatformarch}.${targetproc}"
   docker build \
-    --build-arg="targetplatform=${baseos}/${targetproc}" \
-    --build-arg="targetarch=${baseos}-${targetarch}" \
-    --tag "${basetag}:${baseDistro}.${baseVersion}.${targetproc}" .
-  docker push "${basetag}:${baseDistro}.${baseVersion}.${targetproc}"
+  --platform="${dockerdefaultplatformos:-$baseos}/${platformarch}" \
+  --build-arg="targetarch=${targetos:-$baseos}-${targetproc}" \
+  --tag "${tag}" . \
+  && builtTags+=("${tag}") \
+  && builtPlatformOs+=("${dockerdefaultplatformos:-$baseos}") \
+  && builtPlatformArch+=("${platformarch}") \
+  && [[ ${nodist} != 1 ]] && docker push ${tag}
 }
 
-# STEP 2 - build & push amd64 and arm64v8 container images
-export targetarch='x64'
-export targetproc='amd64'
-build_image
+if [[ $noamd64 != 1 ]]; then
+  export dockerdefaultplatformarch='amd64'
+  export platformarch='x86_64'
+  export targetproc='x64'
+  build_image
+fi
 
-export targetarch='arm64'
-export targetproc="${targetarch}"
-build_image
+if [[ $noarm64 != 1 ]]; then
+  export dockerdefaultplatformarch='arm64'
+  export platformarch='arm64'
+  export targetproc='x64'
+  build_image
+fi
 
-# STEP 3 - create and push manifest
-docker manifest create --amend "${basetag}:latest" "${basetag}:${baseDistro}.${baseVersion}.amd64" "${basetag}:${baseDistro}.${baseVersion}.arm64"
-docker manifest annotate "${basetag}:latest" "${basetag}:${baseDistro}.${baseVersion}.amd64" --os "${baseos}" --arch amd64
-docker manifest annotate "${basetag}:latest" "${basetag}:${baseDistro}.${baseVersion}.arm64" --os "${baseos}" --arch arm64 --variant v8
-docker manifest push --purge "${basetag}:latest"
+# Output built Images
+echo "Images built: \n ${builtTags[@]}"
+
+# create and push Manifest
+if [[ $nodist != 1 ]]; then
+  docker manifest create "${dockerrepository}:latest" "${builtTags[@]}"
+  i=1
+  for tag in "${builtTags[@]}"; do
+    docker manifest annotate "${dockerrepository}:latest" "${tag}" --os "${builtPlatformOs[$i]}" --arch "${builtPlatformArch[$i]}"
+    i=${i}+1
+  done
+  docker manifest push --purge "${dockerrepository}:latest"
+fi
