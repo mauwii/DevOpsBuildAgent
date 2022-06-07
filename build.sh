4751f0b94c5f376486934c7866a1c49014350cdc
Mauwii <mauwii@outlook.de>
2022-06-07 22:51:01 +0200

    update build.sh * add option to update readme on Dockerhub   * needs your password to work * correctly compare numbers in conditions

--- build.sh
+++ build.sh
@@ -4,11 +4,25 @@ set -e
 # check command arguments if exists
 for argument in "$@"; do
   # Disable Distribution - usefull when experimenting locally
-  [[ "$argument" = "--nodist" ]] && export nodist=1 && echo "distribution will be skipped \n"
+  if [[ "$argument" == "--nodist" ]]; then
+    export nodist=1
+    echo -e "\ndistribution will be skipped - have fun experimenting ;)\n"
+  fi
   # Dont build amd64 - usefull since this one is already working
-  [[ "$argument" = "--noamd64" ]] && export noamd64=1 && echo "Not building amd64 \n"
+  if [[ "$argument" == "--noamd64" ]]; then
+    export noamd64=1
+    echo -e "will skip building amd64\n"
+  fi
   # Dont build arm64 - would be unfair to only opt-out amd64
-  [[ "$argument" = "--noarm64" ]] && export noarm64=1 && echo "Not building amd64 \n"
+  if [[ "$argument" == "--noarm64" ]]; then
+    export noarm64=1
+    echo -e "will skip building arm64\n"
+  fi
+  # Update the Readme located on Dockerhub
+  if [[ "$argument" == "--updateReadme" ]]; then
+    export updateReadme=1
+    echo -e "Will update Readme if previous Steps did not fail\n"
+  fi
 done
 
 # Declare base-env
@@ -27,24 +41,25 @@ export builtPlatformArch=()
 build_image() {
   export DOCKER_DEFAULT_PLATFORM="${dockerdefaultplatformos:-$baseos}/${dockerdefaultplatformarch}"
   export tag="${dockerrepository}:${dockerdefaultplatformos:-$baseos}.${baseDistro}.${baseVersion}.${dockerdefaultplatformarch}.${targetproc}"
+  echo -e "going to build ${tag}\n"
   docker build \
-  --platform="${dockerdefaultplatformos:-$baseos}/${platformarch}" \
-  --build-arg="targetarch=${targetos:-$baseos}-${targetproc}" \
-  --tag "${tag}" . \
+    --platform="${dockerdefaultplatformos:-$baseos}/${platformarch}" \
+    --build-arg="targetarch=${targetos:-$baseos}-${targetproc}" \
+    --tag "${tag}" . \
   && builtTags+=("${tag}") \
   && builtPlatformOs+=("${dockerdefaultplatformos:-$baseos}") \
   && builtPlatformArch+=("${platformarch}") \
-  && [[ ${nodist} != 1 ]] && docker push ${tag}
+  && [[ $nodist -ne 1 ]] && docker push ${tag}
 }
 
-if [[ $noamd64 != 1 ]]; then
+if [[ $noamd64 -ne 1 ]]; then
   export dockerdefaultplatformarch='amd64'
   export platformarch='x86_64'
   export targetproc='x64'
   build_image
 fi
 
-if [[ $noarm64 != 1 ]]; then
+if [[ $noarm64 -ne 1 ]]; then
   export dockerdefaultplatformarch='arm64'
   export platformarch='arm64'
   export targetproc='x64'
@@ -52,10 +67,10 @@ if [[ $noarm64 != 1 ]]; then
 fi
 
 # Output built Images
-echo "Images built: \n ${builtTags[@]}"
+[[ ${#builtTags[@]} -gt 0 ]] && echo -e "Images built:\n ${builtTags[@]}" || echo -e "Did not build any image."
 
 # create and push Manifest
-if [[ $nodist != 1 ]]; then
+if [[ $nodist -ne 1 ]]; then
   docker manifest create "${dockerrepository}:latest" "${builtTags[@]}"
   i=1
   for tag in "${builtTags[@]}"; do
@@ -63,4 +78,15 @@ if [[ $nodist != 1 ]]; then
     i=${i}+1
   done
   docker manifest push --purge "${dockerrepository}:latest"
+fi
+
+# Update Readme in Docker hub - needs your password in env as $dockersecret
+if [[ $updateReadme -eq 1 ]]; then
+  export DOCKER_DEFAULT_PLATFORM=linux/amd64
+  docker run --rm -v $PWD:/workspace \
+    -e DOCKERHUB_USERNAME="${dockerhubuser}" \
+    -e DOCKERHUB_PASSWORD="${dockersecret}" \
+    -e DOCKERHUB_REPOSITORY="${dockerrepository}" \
+    -e README_FILEPATH='/workspace/README.md' \
+    peterevans/dockerhub-description:2.1.0
 fi
