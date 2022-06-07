1d19be1032222e9c273cf4f592796a439884c450
Mauwii <mauwii@outlook.de>
2022-06-07 19:50:22 +0200

    update run-local.sh

--- run-local.sh
+++ run-local.sh
@@ -1,19 +1,29 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+set -e
 
-export build="ubuntu.20.04"
+export baseos='linux'
+export baseDistro='ubuntu'
+export baseVersion='20.04'
+export dockerregistry='docker.io'
+export dockerhubuser='mauwii'
+export dockerimage='devopsbuildagent'
+export targetproc='x64'
+export dockerrepository="${dockerhubuser}/${dockerimage}"
 
 # if on Apple M1 but still want to use amd64, just add x64 to script execution
 # ./run-local.sh x64 - then M1 will be ignored
-
 if [[ "$(arch)" = "arm64" ]] && [[ $1 != "x64" ]]; then
-  export DOCKER_DEFAULT_PLATFORM="linux/arm64"
-  export tag="${build}.arm64"
+  export dockerdefaultplatformarch='arm64'
 else
-  export DOCKER_DEFAULT_PLATFORM="linux/amd64"
-  export tag="${build}.amd64"
+  export dockerdefaultplatformarch='amd64'
 fi
 
-echo "using tag ${tag}"
+export DOCKER_DEFAULT_PLATFORM="${baseos}/${dockerdefaultplatformarch}"
+export tag="${baseos}.${baseDistro}.${baseVersion}.${dockerdefaultplatformarch}.${targetproc}"
+export dockerimage="${dockerrepository}:${tag}"
+
+echo "going to run ${dockerimage}\n"
+
 docker run --rm \
   --env-file ./.env \
-  "docker.io/mauwii/devopsbuildagent:${tag}"
+  "${dockerrepository}:${tag}"
