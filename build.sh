05219d51140e0e92a99db9a14ae3f5fdb17b712a
Mauwii <mauwii@outlook.de>
2022-06-14 21:47:24 +0200

    stop exporting all variables

--- build.sh
+++ build.sh
@@ -5,46 +5,46 @@ set -e
 for argument in "$@"; do
   # Disable Distribution - usefull when experimenting locally
   if [[ "$argument" == "--nodist" ]]; then
-    export nodist=1
+    nodist=1
     echo -e "\ndistribution will be skipped - have fun experimenting ;)\n"
   fi
   # Dont build amd64 - usefull since this one is already working
   if [[ "$argument" == "--noamd64" ]]; then
-    export noamd64=1
+    noamd64=1
     echo -e "will skip building amd64\n"
   fi
   # Dont build arm64 - would be unfair to only opt-out amd64
   if [[ "$argument" == "--noarm64" ]]; then
-    export noarm64=1
+    noarm64=1
     echo -e "will skip building arm64\n"
   fi
   # Update the Readme located on Dockerhub
   if [[ "$argument" == "--updateReadme" ]]; then
-    export updateReadme=1
+    updateReadme=1
     echo -e "Will update Readme if previous Steps did not fail\n"
   fi
   # can be used to not loose current prod/dev containers built by pipeline
   if [[ "$argument" == "--local" ]]; then
-    export DEVTAG=local
+    DEVTAG=local
   fi
 done
 
 # Declare base-env
-export baseOS='linux'
-export baseDistro='ubuntu'
-export baseVersion='20.04'
-export dockerRegistry='docker.io'
-export dockerhubuser='mauwii'
-export dockerimage='devopsbuildagent'
-export dockerrepository="${dockerhubuser}/${dockerimage}"
-export builtTags=()
-export builtPlatformOs=()
-export builtPlatformArch=()
+baseOS='linux'
+baseDistro='ubuntu'
+baseVersion='20.04'
+dockerRegistry='docker.io'
+dockerhubuser='mauwii'
+dockerimage='devopsbuildagent'
+dockerrepository="${dockerhubuser}/${dockerimage}"
+builtTags=()
+builtPlatformOs=()
+builtPlatformArch=()
 
 # create build function
 build_image() {
-  export DOCKER_DEFAULT_PLATFORM="${dockerdefaultplatformos:-$baseOS}/${BASEARCH}${BASEARCHVARIANT:+$BASEARCHVARIANT}"
-  export tag="${dockerrepository}:${dockerdefaultplatformos:-$baseOS}.${baseDistro}.${baseVersion}.${BASEARCH}${BASEARCHVARIANT:+$BASEARCHVARIANT}${DEVTAG:+.$DEVTAG}"
+  DOCKER_DEFAULT_PLATFORM="${dockerdefaultplatformos:-$baseOS}/${BASEARCH}${BASEARCHVARIANT:+$BASEARCHVARIANT}"
+  tag="${dockerrepository}:${dockerdefaultplatformos:-$baseOS}.${baseDistro}.${baseVersion}.${BASEARCH}${BASEARCHVARIANT:+$BASEARCHVARIANT}${DEVTAG:+.$DEVTAG}"
   echo -e "going to build ${tag}\n"
   docker build \
     --platform="${baseOS}/${BASEARCH}${BASEARCHVARIANT:+/$BASEARCHVARIANT}" \
@@ -59,17 +59,15 @@ build_image() {
 }
 
 if [[ $noamd64 -ne 1 ]]; then
-  export BASEARCH='amd64'
-  # export platformarch='x86_64'
-  export targetproc='x64'
+  BASEARCH='amd64'
+  targetproc='x64'
   build_image
 fi
 
 if [[ $noarm64 -ne 1 ]]; then
-  export BASEARCH='arm64'
-  export BASEARCHVARIANT='v8'
-  # export platformarch='arm64'
-  export targetproc="${BASEARCH}"
+  BASEARCH='arm64'
+  BASEARCHVARIANT='v8'
+  targetproc="${BASEARCH}"
   build_image
 fi
 
@@ -89,7 +87,7 @@ fi
 
 # Update Readme in Docker hub - needs your password in env as $dockersecret
 if [[ $updateReadme -eq 1 ]]; then
-  export DOCKER_DEFAULT_PLATFORM=linux/amd64
+  DOCKER_DEFAULT_PLATFORM=linux/amd64
   docker run --rm -v $PWD:/workspace \
     -e DOCKERHUB_USERNAME="${dockerhubuser}" \
     -e DOCKERHUB_PASSWORD="${dockersecret}" \
