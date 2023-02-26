5e0d96b3ddbd097d8ad81d7b75b84228347db996
mauwii <Mauwii@outlook.de>
2023-02-26 19:20:35 +0100

    use BuildKit

--- Dockerfile
+++ Dockerfile
@@ -1,41 +1,64 @@
-ARG BASEARCH=amd64
-FROM ${BASEARCH}/ubuntu:20.04
+# syntax=docker/dockerfile:1
+FROM --platform=${TARGETPLATFORM} ubuntu:20.04
 
-RUN echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
-RUN DEBIAN_FRONTEND=noninteractive apt-get update
-RUN DEBIAN_FRONTEND=noninteractive apt-get upgrade
+# Prepare apt for buildkit cache
+RUN rm -f /etc/apt/apt.conf.d/docker-clean \
+  && echo 'Binary::apt::APT::Keep-Downloaded-Packages "true";' >/etc/apt/apt.conf.d/keep-cache
+
+# Prepare pip for buildkit cache
+ARG PIP_CACHE_DIR=/var/cache/buildkit/pip
+ENV PIP_CACHE_DIR ${PIP_CACHE_DIR}
+RUN mkdir -p ${PIP_CACHE_DIR}
+
+ARG DEBIAN_FRONTEND=noninteractive
+RUN echo 'APT::Get::Assume-Yes "true";' >/etc/apt/apt.conf.d/90assumeyes
 
 # Can be 'linux-x64', 'linux-arm64', 'linux-arm', 'rhel.6-x64'.
-ARG targetos
-ARG targetproc
-ENV TARGETARCH=${targetos:-linux}-${targetproc:-x64}
+ARG targetos=linux
+ARG targetproc=x64
 
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -y -qq --no-install-recommends \
-  apt-transport-https \
-  apt-utils \
-  ca-certificates \
-  curl \
-  git \
-  iputils-ping \
-  jq \
-  lsb-release \
-  software-properties-common \
-  unzip \
-  zip
+RUN \
+  --mount=type=cache,target=/var/cache/apt,sharing=locked \
+  --mount=type=cache,target=/var/lib/apt,sharing=locked \
+  apt-get update \
+  && apt-get install -y -qq --no-install-recommends \
+    apt-transport-https \
+    apt-utils \
+    ca-certificates \
+    curl \
+    git \
+    iputils-ping \
+    jq \
+    lsb-release \
+    software-properties-common \
+    unzip \
+    zip
 
 # Install Azure-CLI v2.37.0
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -y -qq --no-install-recommends \
+RUN \
+  --mount=type=cache,target=/var/cache/apt,sharing=locked \
+  --mount=type=cache,target=/var/lib/apt,sharing=locked \
+  --mount=type=cache,target=${PIP_CACHE_DIR} \
+  apt-get update \
+  && apt-get install -y -qq --no-install-recommends \
     gnupg \
-    python3.8-dev \
+    python3-dev \
     python3-pip \
     gcc \
-  && pip3 install --upgrade pip --force \
-  && pip3 install azure-cli==2.37.0 \
-  && DEBIAN_FRONTEND=noninteractive apt-get remove -y python3-pip \
-  && DEBIAN_FRONTEND=noninteractive apt-get autoremove -y
+  && pip3 install \
+    --upgrade \
+    --force \
+    pip \
+  && pip3 install \
+    --upgrade \
+    --force \
+    azure-cli==2.37.0 \
+  && apt-get remove -y \
+    python3-pip \
+  && apt-get autoremove -y
 
-# Downloading and installing Powershell for specified targetproc (linux-x64 per default)
-RUN curl -L -o /tmp/powershell.tar.gz "https://github.com/PowerShell/PowerShell/releases/download/v7.2.4/powershell-7.2.4-${TARGETARCH}.tar.gz" \
+# Downloading and installing Powershell for specified TARGETARCH (linux-x64 per default)
+RUN curl -L -o /tmp/powershell.tar.gz "https://github.com/PowerShell/PowerShell/releases/download/v7.2.4/powershell-7.2.4-${targetos}-${targetproc}.tar.gz" \
   && mkdir -p /opt/microsoft/powershell/7 \
   && tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7 \
   && chmod +x /opt/microsoft/powershell/7/pwsh \
@@ -45,32 +68,9 @@ RUN curl -L -o /tmp/powershell.tar.gz "https://github.com/PowerShell/PowerShell/
 RUN /usr/bin/pwsh -Command Set-PSRepository -Name PSGallery -InstallationPolicy Trusted \
   && /usr/bin/pwsh -Command Install-Module -Name Az -RequiredVersion 8.0.0 -Scope AllUsers
 
-# Create Python3.8.10 tool directory, upgrade pip
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -y -qq --no-install-recommends \
-    python3.8-venv \
-  && mkdir -p /azp/_work/_tool/Python/3.8.10 \
-  && cd /azp/_work/_tool/Python/3.8.10 \
-  && python3.8 -m venv x64 \
-  && touch x64.complete \
-  && . /azp/_work/_tool/Python/3.8.10/x64/bin/activate \
-  && pip3 install --upgrade pip \
-  && deactivate
-
-# Create Python3.9.5 tool directory, upgrade pip
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -y -qq --no-install-recommends \
-    python3.9-dev \
-    python3.9-venv \
-  && mkdir -p /azp/_work/_tool/Python/3.9.5 \
-  && cd /azp/_work/_tool/Python/3.9.5 \
-  && python3.8 -m venv x64 \
-  && touch x64.complete \
-  && . /azp/_work/_tool/Python/3.9.5/x64/bin/activate \
-  && pip3 install --upgrade pip\
-  && deactivate
-
+# Setup Runtime
+ENV TARGETARCH ${targetos}-${targetproc}
 WORKDIR /azp
-
 COPY ./start.sh .
 RUN chmod +x start.sh
-
-ENTRYPOINT [ "/usr/bin/env", "bash", "./start.sh" ]
+ENTRYPOINT [ "start.sh" ]
--- build.sh
+++ build.sh
@@ -1,41 +1,44 @@
 #!/usr/bin/env bash
 set -e
 
+# Enable BuildKit
+export DOCKER_BUILDKIT=1
+
 # check command arguments if exists
 for argument in "$@"; do
-  # Disable Distribution - usefull when experimenting locally
-  if [[ "$argument" == "--nodist" ]]; then
-    nodist=1
-    echo -e "\ndistribution will be skipped - have fun experimenting ;)\n"
-  fi
-  # Dont build amd64 - usefull since this one is already working
-  if [[ "$argument" == "--noamd64" ]]; then
-    noamd64=1
-    echo -e "will skip building amd64\n"
-  fi
-  # Dont build arm64 - would be unfair to only opt-out amd64
-  if [[ "$argument" == "--noarm64" ]]; then
-    noarm64=1
-    echo -e "will skip building arm64\n"
-  fi
-  # Update the Readme located on Dockerhub
-  if [[ "$argument" == "--updateReadme" ]]; then
-    updateReadme=1
-    echo -e "Will update Readme if previous Steps did not fail\n"
-  fi
-  # can be used to not loose current prod/dev containers built by pipeline
-  if [[ "$argument" == "--local" ]]; then
-    DEVTAG=local
-  fi
+    # Disable Distribution - usefull when experimenting locally
+    if [[ "$argument" == "--nodist" ]]; then
+        nodist=1
+        echo -e "\ndistribution will be skipped - have fun experimenting ;)\n"
+    fi
+    # Dont build amd64 - usefull since this one is already working
+    if [[ "$argument" == "--noamd64" ]]; then
+        noamd64=1
+        echo -e "will skip building amd64\n"
+    fi
+    # Dont build arm64 - would be unfair to only opt-out amd64
+    if [[ "$argument" == "--noarm64" ]]; then
+        noarm64=1
+        echo -e "will skip building arm64\n"
+    fi
+    # Update the Readme located on Dockerhub
+    if [[ "$argument" == "--updateReadme" ]]; then
+        updateReadme=1
+        echo -e "Will update Readme if previous Steps did not fail\n"
+    fi
+    # can be used to not loose current prod/dev containers built by pipeline
+    if [[ "$argument" == "--local" ]]; then
+        DEVTAG=local
+    fi
 done
 
 # Declare base-env
-baseOS='linux'
-baseDistro='ubuntu'
-baseVersion='20.04'
-dockerRegistry='docker.io'
-dockerhubuser='mauwii'
-dockerimage='devopsbuildagent'
+baseOS="${baseOS:-linux}"
+baseDistro="${baseDistro:-ubuntu}"
+baseVersion="${baseVersion:-20.04}"
+dockerRegistry="${dockerRegistry:-docker.io}"
+dockerhubuser="${dockerhubuser:-"$(whoami)"}"
+dockerimage="${dockerimage:-devopsbuildagent}"
 dockerrepository="${dockerhubuser}/${dockerimage}"
 builtTags=()
 builtPlatformOs=()
@@ -43,52 +46,60 @@ builtPlatformArch=()
 
 # create build function
 build_image() {
-  DOCKER_DEFAULT_PLATFORM="${dockerdefaultplatformos:-$baseOS}/${BASEARCH}${BASEARCHVARIANT:+/$BASEARCHVARIANT}"
-  tag="${dockerrepository}:${dockerdefaultplatformos:-$baseOS}.${baseDistro}.${baseVersion}.${BASEARCH}${BASEARCHVARIANT:+$BASEARCHVARIANT}${DEVTAG:+.$DEVTAG}"
-  echo -e "going to build ${tag}\n"
-  docker build \
-    --build-arg="BASEARCH=${BASEARCH}${BASEARCHVARIANT:+$BASEARCHVARIANT}" \
-    --build-arg="targetos=${targetos:-$baseOS}" \
-    --build-arg="targetproc=${targetproc}" \
-    --tag "${tag}" . \
-  && builtTags+=("${tag}") \
-  && builtPlatformOs+=("${dockerdefaultplatformos:-$baseOS}") \
-  && builtPlatformArch+=("${BASEARCH:+--arch $BASEARCH}${BASEARCHVARIANT:+ --variant $BASEARCHVARIANT}") \
-  && [[ $nodist -ne 1 ]] && docker push ${tag} || echo
-    # --platform="${baseOS}/${BASEARCH}${BASEARCHVARIANT:+/$BASEARCHVARIANT}" \
+    # TARGETPLATFORM="${dockerdefaultplatformos:-$baseOS}/${BASE_ARCH}${BASE_ARCH_VARIANT:+/$BASE_ARCH_VARIANT}"
+    tag="${dockerrepository}:${dockerdefaultplatformos:-$baseOS}.${baseDistro}.${baseVersion}.${BASE_ARCH}${BASE_ARCH_VARIANT:+$BASE_ARCH_VARIANT}${DEVTAG:+.$DEVTAG}"
+    echo -e "going to build ${tag}\n"
+    docker build \
+        --platform="${dockerdefaultplatformos:-$baseOS}/${BASE_ARCH}${BASE_ARCH_VARIANT:+/$BASE_ARCH_VARIANT}" \
+        --build-arg="BASE_ARCH=${BASE_ARCH}${BASE_ARCH_VARIANT:+$BASE_ARCH_VARIANT}" \
+        --build-arg="targetos=${targetos:-$baseOS}" \
+        --build-arg="targetproc=${targetproc:-x64}" \
+        --tag="${tag}" . \
+        && builtTags+=("${tag}") \
+        && builtPlatformOs+=("${dockerdefaultplatformos:-$baseOS}") \
+        && builtPlatformArch+=("${BASE_ARCH:+--arch $BASE_ARCH}${BASE_ARCH_VARIANT:+ --variant $BASE_ARCH_VARIANT}") \
+        && (
+            [[ $nodist -ne 1 ]] \
+                && docker push "${tag}" \
+                || echo "did not push ${tag}"
+        )
 }
 
 # Build amd64 image if not disabled
 if [[ $noamd64 -ne 1 ]]; then
-  BASEARCH='amd64' targetproc='x64' build_image
+    BASE_ARCH='amd64' targetproc='x64' build_image
 fi
 
 # build arm64v8 if not disabled
 if [[ $noarm64 -ne 1 ]]; then
-  BASEARCH='arm64' BASEARCHVARIANT='v8' targetproc="${BASEARCH}" build_image
+    BASE_ARCH='arm64' BASE_ARCH_VARIANT='v8' targetproc='arm64' build_image
 fi
 
 # Output built Images
-[[ ${#builtTags[@]} -gt 0 ]] && echo -e "Images built:\n ${builtTags[@]}" || echo -e "Did not build any image."
+[[ ${#builtTags[@]} -gt 0 ]] \
+    && echo -e "Images built:\n ${builtTags[*]}" \
+    || echo -e "Did not build any image."
 
 # create and push Manifest
 if [[ $nodist -ne 1 ]]; then
-  docker manifest create "${dockerrepository}:latest" "${builtTags[@]}"
-  i=1
-  for tag in "${builtTags[@]}"; do
-    docker manifest annotate "${dockerrepository}:latest" "${tag}" --os "${builtPlatformOs[$i]}" ${builtPlatformArch[$i]}
-    i=${i}+1
-  done
-  docker manifest push --purge "${dockerrepository}:latest"
+    docker manifest create "${dockerrepository}:latest" "${builtTags[@]}"
+    i=1
+    for tag in "${builtTags[@]}"; do
+        docker manifest annotate "${dockerrepository}:latest" "${tag}" --os "${builtPlatformOs[$i]}" ${builtPlatformArch[$i]}
+        i=${i}+1
+    done
+    docker manifest push --purge "${dockerrepository}:latest"
 fi
 
 # Update Readme in Docker hub - needs your password in env as $dockersecret
-if [[ $updateReadme -eq 1 ]]; then
-  DOCKER_DEFAULT_PLATFORM=linux/amd64
-  docker run --rm -v $PWD:/workspace \
-    -e DOCKERHUB_USERNAME="${dockerhubuser}" \
-    -e DOCKERHUB_PASSWORD="${dockersecret}" \
-    -e DOCKERHUB_REPOSITORY="${dockerrepository}" \
-    -e README_FILEPATH='/workspace/README.md' \
-    peterevans/dockerhub-description:2.1.0
-fi
+[[ $updateReadme -eq 1 && -n $dockersecret ]] \
+    && DOCKER_DEFAULT_PLATFORM=linux/amd64 \
+        docker run \
+        --rm \
+        -v "$(pwd)":/workspace \
+        -e DOCKERHUB_USERNAME="${dockerhubuser}" \
+        -e DOCKERHUB_PASSWORD="${dockersecret}" \
+        -e DOCKERHUB_REPOSITORY="${dockerrepository}" \
+        -e README_FILEPATH='/workspace/README.md' \
+        peterevans/dockerhub-description:2.1.0 \
+    || echo "did not update Readme"
